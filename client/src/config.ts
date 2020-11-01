ac01e0b2b6cdfe590c490bcc36f78e9441e2ea27
Dilip Agheda <dilip_agheda@yahoo.com>
2020-11-01 20:55:43 +1100

    updated react client config

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,10 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
+// Once your application is deployed, copy an API id here so that the frontend could interact with it
 const apiId = 'tqfsm107kd'
 const region = 'ap-southeast-2'
 export const apiEndpoint = `https://${apiId}.execute-api.${region}.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map
+  // Create an Auth0 application and copy values from it into this map
   domain: 'dev-o6dlizfb.au.auth0.com', // Auth0 domain
   clientId: 'nGdcelHUjlTmqYY2iPfPpZCMXwc4Dhhw', // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
